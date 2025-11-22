3f3850bba2f4b22b55925bd231a7f09db21f4b84
felipe panosso <panosso@localhost.localdomain>
2025-11-21 23:35:09 -0300

    feat: implement database connection and update product table schema

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/FelipePn10/ecomms/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":6800",
-		db:   dbConfig{},
-	}
-
-	api := application{
-		config: cfg,
+		db: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=postgres sslmode=disable"),
+		},
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	slog.SetDefault(logger)
 
-	err := api.run(api.mount())
+	// Database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
 	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("database connected successfully")
+
+	api := application{
+		config: cfg,
+	}
+
+	if err := api.run(api.mount()); err != nil {
 		slog.Error("could not start server", "error", err)
 		os.Exit(1)
 	}
--- internal/adapters/postgresql/migrations/00001_create_products.sql
+++ internal/adapters/postgresql/migrations/00001_create_products.sql
@@ -4,7 +4,7 @@ CREATE TABLE products (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT,
-    price_in_cents INTEGER NOT NULL CHECK (price >= 0),
+    price_in_cents INTEGER NOT NULL CHECK (price_in_cents >= 0),
     quantity INTEGER NOT NULL CHECK (quantity >= 0),
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
